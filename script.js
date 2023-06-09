190112f352d927fef6ebee5fe57125d4305c1d40
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-09 04:41:55 -0700

    created a submission

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function rot13(encodedStr) {
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.write(rot13("SERR YBIR? NPPVBWBO"));
+// console.write(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
